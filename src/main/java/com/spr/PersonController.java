725c2583bdb0cb8982edc81f353a738054badb0a
Atul Jha <mail.atuljha@gmail.com>
2022-04-21 16:30:10 +0530

    commit

--- src/main/java/com/spr/PersonController.java
+++ src/main/java/com/spr/PersonController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,6 +19,20 @@ public class PersonController {
 	@RequestMapping("/")
 	public List<Person> getAllRecords(){
 		return personService.getAllPersons();
+	}
+	
+	@RequestMapping("/getPerson")
+	public ModelAndView getPerson() {
+		return new ModelAndView("getPerson");
+	}
+	@RequestMapping("/getById")
+	public Optional<Person> getPersonById(@RequestParam("pid")String id){
+		return personService.getPerson(Integer.parseInt(id));
+	}
+	
+	@RequestMapping("/menu")
+	public ModelAndView menu() {
+		return new ModelAndView("menu");
 	}
 	
 	@RequestMapping("/myForm")
@@ -46,4 +62,23 @@ public class PersonController {
         return personService.getAllPersons();
     }
 	
+	@GetMapping({"/list","/"})
+	public ModelAndView getAllPerson() {
+		return new ModelAndView("list-persons").addObject("persons",personService.getAllPersons());
+	}
+	
+	@RequestMapping(value="update",method = RequestMethod.POST)
+	public ModelAndView updated(@ModelAttribute Person p) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("validate");
+		personService.addPerson(p);
+		System.out.println(p.getPname()+" "+p.getPjob());
+		modelAndView.addObject("user",p);
+		return modelAndView;
+	}
+	@RequestMapping(value = "/getPerson/{id}")
+	public ModelAndView update(@PathVariable("id")String id) {
+		Optional<Person> person=personService.getPerson(Integer.parseInt(id));
+		return new ModelAndView("updatePerson").addObject("person",person.get());
+	}
 }
